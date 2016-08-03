0abbecd2ecb018c5bba4707125bca4e43304fdfc
Alec Ananian <alec.ananian@warnerbros.com>
2016-08-03 15:33:21 -0700

    Updated to latest version of NextGenDataLoader

--- Example/NextGenExample/NextGenDataLoader.swift
+++ Example/NextGenExample/NextGenDataLoader.swift
@@ -6,31 +6,37 @@ import Foundation
 import GoogleMaps
 import NextGenDataManager
 
-class NextGenDataLoader {
+@objc class NextGenDataLoader: NSObject, NextGenHookDelegate {
     
-    static let ManifestData = [[
-        "title": "Man of Steel",
-        "image": "MOS-Onesheet",
-        "manifest": "Data/Manifests/mos_hls_manifest_r60-v0.5",
-        "appdata": "Data/Manifests/mos_appdata_locations_r60-v0.5"
-    ], [
-        "title": "Batman v Superman",
-        "image": "BvS-Onesheet",
-        "manifest": "Data/Manifests/bvs_manifest_r60-v1.1",
-        "appdata": "Data/Manifests/bvs_appdata_locations_r60-v1.1"
-    ], [
-        "title": "Sisters",
-        "image": "Sisters-Onesheet",
-        "manifest": "Data/Manifests/sisters_hls_manifest_v2-R60-generated-spec1.5"
-    ], [
-        "title": "Sisters (Unrated)",
-        "image": "SistersUnrated-Onesheet",
-        "manifest": "Data/Manifests/sisters_extended_hls_manifest_v3-generated-spec1.5"
-    ], [
-        "title": "Minions",
-        "image": "Minions-Onesheet",
-        "manifest": "Data/Manifests/minions_hls_manifest_v6-R60-generated-spec1.5"
-    ]]
+    static let ManifestData = [
+        "urn:dece:cid:eidr-s:DAFF-8AB8-3AF0-FD3A-29EF-Q": [
+            "title": "Man of Steel",
+            "image": "MOS-Onesheet",
+            "manifest": "Data/Manifests/mos_hls_manifest_r60-v0.5",
+            "appdata": "Data/Manifests/mos_appdata_locations_r60-v0.5"
+        ],
+        "urn:dece:cid:eidr-s:B257-8696-871C-A12B-B8C1-S": [
+            "title": "Batman v Superman",
+            "image": "BvS-Onesheet",
+            "manifest": "Data/Manifests/bvs_manifest_r60-v1.1",
+            "appdata": "Data/Manifests/bvs_appdata_locations_r60-v1.1"
+        ],
+        "1": [
+            "title": "Sisters",
+            "image": "Sisters-Onesheet",
+            "manifest": "Data/Manifests/sisters_hls_manifest_v2-R60-generated-spec1.5"
+        ],
+        "2": [
+            "title": "Sisters (Unrated)",
+            "image": "SistersUnrated-Onesheet",
+            "manifest": "Data/Manifests/sisters_extended_hls_manifest_v3-generated-spec1.5"
+        ],
+        "3": [
+            "title": "Minions",
+            "image": "Minions-Onesheet",
+            "manifest": "Data/Manifests/minions_hls_manifest_v6-R60-generated-spec1.5"
+        ]
+    ]
     
     private struct ConfigKey {
         static let TheTakeAPI = "thetake_api_key"
@@ -38,7 +44,17 @@ class NextGenDataLoader {
         static let GoogleMapsAPI = "google_maps_api_key"
     }
     
-    static func loadConfig() {
+    
+    static let sharedInstance = NextGenDataLoader()
+    private var currentCid: String?
+    
+    override init() {
+        super.init()
+        
+        NextGenHook.delegate = self
+    }
+    
+    func loadConfig() {
         // Load configuration file
         if let configDataPath = NSBundle.mainBundle().pathForResource("Data/config", ofType: "json") {
             do {
@@ -65,57 +81,78 @@ class NextGenDataLoader {
         }
     }
     
-    static func loadTitle(titleIndex: Int) {
-        let titleData = ManifestData[titleIndex]
-        
-        // Load current Manifest file
-        if let manifestXMLPath = NSBundle.mainBundle().pathForResource(titleData["manifest"], ofType: "xml") {
-            do {
-                try NGDMManifest.sharedInstance.loadManifestXMLFile(manifestXMLPath)
-                NGDMManifest.sharedInstance.mainExperience?.appearance = NGDMAppearance(type: .Main)
-                
-                NGDMManifest.sharedInstance.inMovieExperience?.appearance = NGDMAppearance(type: .InMovie)
-                NGDMManifest.sharedInstance.outOfMovieExperience?.appearance = NGDMAppearance(type: .OutOfMovie)
-                
-                if TheTakeAPIUtil.sharedInstance.apiKey != nil, let mediaId = NGDMManifest.sharedInstance.mainExperience?.customIdentifier(Namespaces.TheTake) {
-                    TheTakeAPIUtil.sharedInstance.mediaId = mediaId
-                    TheTakeAPIUtil.sharedInstance.prefetchProductFrames(start: 0)
-                    TheTakeAPIUtil.sharedInstance.prefetchProductCategories()
+    func loadTitle(cid: String) {
+        if let titleData = NextGenDataLoader.ManifestData[cid] {
+            // Load current Manifest file
+            if let manifestXMLPath = NSBundle.mainBundle().pathForResource(titleData["manifest"], ofType: "xml") {
+                do {
+                    NGDMManifest.createInstance()
+                    
+                    try NGDMManifest.sharedInstance.loadManifestXMLFile(manifestXMLPath)
+                    NGDMManifest.sharedInstance.mainExperience?.appearance = NGDMAppearance(type: .Main)
+                    
+                    NGDMManifest.sharedInstance.inMovieExperience?.appearance = NGDMAppearance(type: .InMovie)
+                    NGDMManifest.sharedInstance.outOfMovieExperience?.appearance = NGDMAppearance(type: .OutOfMovie)
+                    
+                    if TheTakeAPIUtil.sharedInstance.apiKey != nil, let mediaId = NGDMManifest.sharedInstance.mainExperience?.customIdentifier(Namespaces.TheTake) {
+                        TheTakeAPIUtil.sharedInstance.mediaId = mediaId
+                        TheTakeAPIUtil.sharedInstance.prefetchProductFrames(start: 0)
+                        TheTakeAPIUtil.sharedInstance.prefetchProductCategories()
+                    }
+                    
+                    if var talentAPIUtil = NGDMConfiguration.talentAPIUtil {
+                        talentAPIUtil.apiId = NGDMManifest.sharedInstance.mainExperience?.customIdentifier(Namespaces.Baseline)
+                    }
+                    
+                    NGDMManifest.sharedInstance.mainExperience?.loadTalent()
+                } catch NGDMError.MainExperienceMissing {
+                    print("Error loading Manifest file: no main Experience found")
+                    abort()
+                } catch NGDMError.InMovieExperienceMissing {
+                    print("Error loading Manifest file: no in-movie Experience found")
+                    abort()
+                } catch NGDMError.OutOfMovieExperienceMissing {
+                    print("Error loading Manifest file: no out-of-movie Experience found")
+                    abort()
+                } catch {
+                    print("Error loading Manifest file: unknown error")
+                    abort()
                 }
-                
-                if var talentAPIUtil = NGDMConfiguration.talentAPIUtil {
-                    talentAPIUtil.apiId = NGDMManifest.sharedInstance.mainExperience?.customIdentifier(Namespaces.Baseline)
-                }
-                
-                NGDMManifest.sharedInstance.mainExperience?.loadTalent()
-            } catch NGDMError.MainExperienceMissing {
-                print("Error loading Manifest file: no main Experience found")
-                abort()
-            } catch NGDMError.InMovieExperienceMissing {
-                print("Error loading Manifest file: no in-movie Experience found")
-                abort()
-            } catch NGDMError.OutOfMovieExperienceMissing {
-                print("Error loading Manifest file: no out-of-movie Experience found")
-                abort()
-            } catch {
-                print("Error loading Manifest file: unknown error")
+            } else {
+                print("No Manifest file found")
                 abort()
             }
+            
+            // Load current AppData file
+            if let appDataXMLPath = NSBundle.mainBundle().pathForResource(titleData["appdata"], ofType: "xml") {
+                do {
+                    NGDMManifest.sharedInstance.appData = try NGDMManifest.sharedInstance.loadAppDataXMLFile(appDataXMLPath)
+                } catch {
+                    print("Error loading AppData file")
+                }
+            } else {
+                print("No AppData file found")
+            }
+            
+            currentCid = cid
         } else {
-            print("No Manifest file found")
+            print("No title found for \(cid)")
             abort()
         }
-        
-        // Load current AppData file
-        if let appDataXMLPath = NSBundle.mainBundle().pathForResource(titleData["appdata"], ofType: "xml") {
-            do {
-                NGDMManifest.sharedInstance.appData = try NGDMManifest.sharedInstance.loadAppDataXMLFile(appDataXMLPath)
-            } catch {
-                print("Error loading AppData file")
-            }
-        } else {
-            print("No AppData file found")
-        }
+    }
+    
+    // MARK: NextGenHookDelegate
+    func nextGenExperienceWillClose() {
+        NGDMManifest.destroyInstance()
+    }
+    
+    func videoPlayerWillClose(mode: VideoPlayerMode) {
+        // Handle end of playback
+    }
+    
+    func getProcessedVideoURL(url: NSURL, mode: VideoPlayerMode, completion: (url: NSURL?) -> Void) {
+        // Handle DRM
+        completion(url: url)
     }
     
 }
