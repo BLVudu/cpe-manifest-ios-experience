9aa14bd42c5800debbc40b346527829f0cbdf45d
Alec Ananian <alec.ananian@warnerbros.com>
2016-09-30 10:15:23 -0700

    NGE-438 - Added microHTML message handler for closing webview

--- NextGen/Shared Controllers/WebViewController.swift
+++ NextGen/Shared Controllers/WebViewController.swift
@@ -5,9 +5,13 @@
 import UIKit
 import WebKit
 
-class WebViewController: UIViewController, WKNavigationDelegate {
+class WebViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
     
     private struct Constants {
+        static let ScriptMessageHandlerName = "microHTMLMessageHandler"
+        static let ScriptMessageAppVisible = "AppVisible"
+        static let ScriptMessageAppShutdown = "AppShutdown"
+        
         static let HeaderButtonWidth: CGFloat = (DeviceType.IS_IPAD ? 125 : 100)
         static let HeaderButtonHeight: CGFloat = (DeviceType.IS_IPAD ? 90 : 50)
         static let HeaderIconPadding: CGFloat = (DeviceType.IS_IPAD ? 30 : 15)
@@ -36,11 +40,15 @@ class WebViewController: UIViewController, WKNavigationDelegate {
         self.navigationController?.isNavigationBarHidden = shouldDisplayFullScreen
         
         let configuration = WKWebViewConfiguration()
+        configuration.userContentController = WKUserContentController()
+        configuration.userContentController.add(self, name: Constants.ScriptMessageHandlerName)
+        
         if #available(iOS 9.0, *) {
             configuration.requiresUserActionForMediaPlayback = false
         } else {
             configuration.mediaPlaybackRequiresUserAction = false
         }
+        
         _webView = WKWebView(frame: self.view.bounds, configuration: configuration)
         self.view.addSubview(_webView)
         
@@ -83,18 +91,31 @@ class WebViewController: UIViewController, WKNavigationDelegate {
         button.titleLabel?.layer.shadowOffset = CGSize(width: 0, height: 1)
         button.titleLabel?.layer.masksToBounds = false
         button.titleLabel?.layer.shouldRasterize = true
-        button.addTarget(self, action: #selector(self.onDone), for: UIControlEvents.touchUpInside)
+        button.addTarget(self, action: #selector(self.close), for: UIControlEvents.touchUpInside)
         self.view.addSubview(button)
     }
 
     // MARK: Actions
-    func onDone() {
+    func close() {
         self.dismiss(animated: true, completion: nil)
     }
     
     // MARK: WKNavigationDelegate
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         decisionHandler(WKNavigationActionPolicy.allow)
+    }
+    
+    // MARK: WKScriptMessageHandler
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if message.name == Constants.ScriptMessageHandlerName {
+            if let messageBody = message.body as? String {
+                if messageBody == Constants.ScriptMessageAppVisible {
+                    
+                } else if messageBody == Constants.ScriptMessageAppShutdown {
+                    close()
+                }
+            }
+        }
     }
 
 }
